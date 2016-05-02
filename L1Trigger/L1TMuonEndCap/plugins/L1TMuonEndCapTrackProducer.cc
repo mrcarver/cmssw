29fa115df9f94889915ef89533a3c337a38ead0f
matthew <mattcarver44@gmail.com>
2016-05-02 14:35:47 -0400

    added first set of changes to reflect usage of neighboring sectors. PhiMemoryImage and PatternRecognition mainly so fare

--- L1Trigger/L1TMuonEndCap/plugins/L1TMuonEndCapTrackProducer.cc
+++ L1Trigger/L1TMuonEndCap/plugins/L1TMuonEndCapTrackProducer.cc
@@ -219,7 +219,7 @@ for(int SectIndex=0;SectIndex<NUM_SECTORS;SectIndex++){//perform TF on all 12 se
 
   PatternOutput Test = DeleteDuplicatePatterns(Pout);
 
-  //PrintQuality(Test.detected);
+  PrintQuality(Test.detected);
 
 
   ///////////////////////////////
@@ -470,16 +470,16 @@ for(int SectIndex=0;SectIndex<NUM_SECTORS;SectIndex++){//perform TF on all 12 se
 
 		if(!ME13 && fabs(eta) > 1.1) {
 		  // // Extra debugging output - AWB 29.03.16
-		  // std::cout << "Input: eBX = " << ebx << ", seBX = " << sebx << ", pt = " << xmlpt*1.4 
-		  // 	    << ", phi = " << AllTracks[fbest].phi << ", eta = " << eta 
-		  // 	    << ", theta = " << AllTracks[fbest].theta << ", sign = " << 1 
-		  // 	    << ", quality = " << mode << ", trackaddress = " << 1 
-		  // 	    << ", sector = " << sector << std::endl;
-		  // std::cout << "Output: BX = " << ebx << ", hwPt = " << outCand.hwPt() << ", hwPhi = " << outCand.hwPhi() 
-		  // 	    << ", hwEta = " << outCand.hwEta() << ", hwSign = " << outCand.hwSign() 
-		  // 	    << ", hwQual = " << outCand.hwQual() << ", link = " << outCand.link()
-		  // 	    << ", processor = " << outCand.processor() 
-		  // 	    << ", trackFinderType = " << outCand.trackFinderType() << std::endl;
+		   std::cout << "Input: eBX = " << ebx << ", seBX = " << sebx << ", pt = " << xmlpt*1.4 
+		    	  << ", phi = " << AllTracks[fbest].phi << ", eta = " << eta 
+		    	  << ", theta = " << AllTracks[fbest].theta << ", sign = " << 1 
+		    	  << ", quality = " << mode << ", trackaddress = " << 1 
+		    	  << ", sector = " << sector << std::endl;
+		   std::cout << "Output: BX = " << ebx << ", hwPt = " << outCand.hwPt() << ", hwPhi = " << outCand.hwPhi() 
+		    	  << ", hwEta = " << outCand.hwEta() << ", hwSign = " << outCand.hwSign() 
+		    	  << ", hwQual = " << outCand.hwQual() << ", link = " << outCand.link()
+		    	  << ", processor = " << outCand.processor() 
+		    	  << ", trackFinderType = " << outCand.trackFinderType() << std::endl;
 			holder.push_back(outPair);
 			thisTrack.set_isGMT( 1 );
 		}
